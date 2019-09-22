e7873d3642087b35bbcb59e2a2650e29bb4cfc33
Sanjeewa Senanayaka <32807886+Jay-Senanayaka@users.noreply.github.com>
2019-09-22 15:26:42 -0500

    Changed generate vector to return a vector. Added function to do selection with median of 3. Works at times, needs debugging

--- Homework1/Homework1/Homework1/Homework1.cpp
+++ Homework1/Homework1/Homework1/Homework1.cpp
@@ -19,6 +19,15 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 
+struct IndexToVal
+{
+	int index, val;
+};
+
+bool Comparator(IndexToVal i, IndexToVal j) {
+	return i.val < j.val;
+}
+
 // Function to partition around a pivot and return the new pivot location
 int partition(vector<int> &A, int left, int right, int pivot) {
 	
@@ -40,13 +49,14 @@ int partition(vector<int> &A, int left, int right, int pivot) {
 }
 
 // Function to generate a vector of disctinct values
-void generateVector(vector<int> &A, int vector_size) {
+vector<int> generateVector( int vector_size) {
+	vector<int> A;
 	cout << "Generation started ..." << endl;
 	//unordered_set<int> in_vector;
 	int random_val;
-	ofstream text_file("100000.txt");
-	if (text_file.is_open()) {
-		text_file << "{";
+	//ofstream text_file("100000.txt");
+	//if (text_file.is_open()) {
+		//text_file << "{";
 		while (A.size() <= vector_size)
 		{
 			srand(static_cast<long unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count()));
@@ -55,17 +65,18 @@ void generateVector(vector<int> &A, int vector_size) {
 			//if (itr == in_vector.end()) {
 				A.push_back(random_val);
 				//in_vector.insert(random_val);
-				text_file << random_val << " ,";
+				//text_file << random_val << " ,";
 			//}
 		}
-		text_file << " }";
-	}
+		//text_file << " }";
+	//}
 	 
 	//in_vector.empty();
 	cout << "Generation completed!" << endl;
+	return A;
 }
 
-int Selection(vector<int> &A, int left, int right, int k) {
+int SelectionWithRandomPivot(vector<int> &A, int left, int right, int k) {
 	int random_pivot = 0;
 	do {
 		random_pivot = left + rand() % right + left;
@@ -78,31 +89,60 @@ int Selection(vector<int> &A, int left, int right, int k) {
 		return A[position];
 	}
 	else if (position + 1 < k) {
-		Selection(A, position, right, k);
+		SelectionWithRandomPivot(A, position, right, k);
 	}
 	else {
-		Selection(A, left, position, k);
+		SelectionWithRandomPivot(A, left, position, k);
 	}
 			
+}
+
+int SelectionWithMedianOfThreePivot(vector<int> &A, int left, int right, int k) {
+	int pivot = 0;
+		
+	IndexToVal arr[] = { {left, A[left]}, {right, A[right]}, {left + right / 2, A[left + right / 2]} };
+
+	sort(arr, arr + 3, Comparator);
+
+	pivot = arr[1].index;
+	int position = partition(A, left, right, pivot);
+
+	if (position + 1 == k) {
+		return A[position];
+	}
+	else if (position + 1 < k) {
+		SelectionWithMedianOfThreePivot(A, position, right, k);
+	}
+	else {
+		SelectionWithMedianOfThreePivot(A, left, position, k);
+	}
+
 }
 
 int main()
 {
 	
-	vector<int> A;
+	vector<int> A, B;
 				
 	int vector_size;
 	cout << "Enter required vector size: ";
 	cin >> vector_size;
-	generateVector(A, vector_size);
-
+	A = generateVector(vector_size);
+	B = A;
+	
 	int k;
 	cout << "Find Kth smallest where K is: ";
 	cin >> k;
 
-	int Kth_smallest = Selection(A, 0, A.size() - 1, k);
+	int Kth_smallest = 0;
+	Kth_smallest = SelectionWithMedianOfThreePivot(B, 0, B.size() - 1, k);
 
-	cout << "Kth smallest is: " << Kth_smallest << endl;
+	cout << "Kth smallest by median of 3 is: " << Kth_smallest << endl;
+
+	Kth_smallest = SelectionWithRandomPivot(A, 0, A.size() - 1, k);
+
+	cout << "Kth smallest by random pivot is : " << Kth_smallest << endl;
+
 	
 }
 
