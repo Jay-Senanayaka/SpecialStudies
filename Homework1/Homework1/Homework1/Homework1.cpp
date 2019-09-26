caa11b369d87d1d5096035f75651264cd4ec3cd1
Sanjeewa Senanayaka <32807886+Jay-Senanayaka@users.noreply.github.com>
2019-09-26 13:31:46 -0500

    Fixed selection by random pivot and median of three.

--- Homework1/Homework1/Homework1/Homework1.cpp
+++ Homework1/Homework1/Homework1/Homework1.cpp
@@ -9,6 +9,7 @@
 #include <random>
 #include <chrono>
 #include<fstream>
+#include <string>
 using namespace std;
 
 // Function to swap 
@@ -19,14 +20,17 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 
+// Structure that keeps the index to value mapping
 struct IndexToVal
 {
 	int index, val;
 };
 
+// A comparator to check 
 bool Comparator(IndexToVal i, IndexToVal j) {
 	return i.val < j.val;
 }
+
 
 // Function to partition around a pivot and return the new pivot location
 int partition(vector<int> &A, int left, int right, int pivot) {
@@ -36,7 +40,7 @@ int partition(vector<int> &A, int left, int right, int pivot) {
 	int pivot_val = A[right];
 	int j = left;
 	for (int i = left; i <= right - 1; i++) {
-		if (A[i] <= pivot_val) {
+		if (A[i] < pivot_val) {
 			swap(&A[i], &A[j]);
 			j++;
 		}
@@ -52,35 +56,33 @@ int partition(vector<int> &A, int left, int right, int pivot) {
 vector<int> generateVector( int vector_size) {
 	vector<int> A;
 	cout << "Generation started ..." << endl;
-	//unordered_set<int> in_vector;
+	
 	int random_val;
-	//ofstream text_file("100000.txt");
-	//if (text_file.is_open()) {
-		//text_file << "{";
-		while (A.size() <= vector_size)
+	string file_name = to_string(vector_size) + ".txt";
+	ofstream text_file;
+	text_file.open(file_name.c_str());
+	if (text_file.is_open()) {
+		text_file << "{";
+		while (A.size() < vector_size)
 		{
-			srand(static_cast<long unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count()));
 			random_val = rand() % (2 * vector_size) + (vector_size);
-			//unordered_set<int>::const_iterator itr = in_vector.find(random_val);
-			//if (itr == in_vector.end()) {
-				A.push_back(random_val);
-				//in_vector.insert(random_val);
-				//text_file << random_val << " ,";
-			//}
+			A.push_back(random_val);
+			text_file << random_val << " ,";
+							
 		}
-		//text_file << " }";
-	//}
+		text_file << " }";
+	}
 	 
-	//in_vector.empty();
 	cout << "Generation completed!" << endl;
 	return A;
 }
 
+// Selection algorithm to find the Kth smallest by using the pivot to be random
 int SelectionWithRandomPivot(vector<int> &A, int left, int right, int k) {
 	int random_pivot = 0;
 	do {
-		random_pivot = left + rand() % right + left;
-	} while (random_pivot >= A.size() || random_pivot > right || random_pivot < left);
+		random_pivot = left + rand() % (right + left);
+	} while (random_pivot >= A.size() | random_pivot > right | random_pivot < left);
 		
 
 	int position = partition(A, left, right, random_pivot);
@@ -89,41 +91,60 @@ int SelectionWithRandomPivot(vector<int> &A, int left, int right, int k) {
 		return A[position];
 	}
 	else if (position + 1 < k) {
-		SelectionWithRandomPivot(A, position, right, k);
+		if (position + 1 <= right) {
+			return SelectionWithRandomPivot(A, position + 1, right, k);
+		}
+		return INT_MAX;
 	}
 	else {
-		SelectionWithRandomPivot(A, left, position, k);
+		if (position - 1 >= left) {
+			return SelectionWithRandomPivot(A, left, position - 1, k);
+		}
+
+		return INT_MIN;
 	}
 			
 }
 
+
+// Selection algorithm to find the Kth smallest by using the pivot to be the median of three
 int SelectionWithMedianOfThreePivot(vector<int> &A, int left, int right, int k) {
 	int pivot = 0;
+
+	int middle = (left + right) / 2;
 		
-	IndexToVal arr[] = { {left, A[left]}, {right, A[right]}, {left + right / 2, A[left + right / 2]} };
+	IndexToVal arr[] = { {left, A[left]}, {right, A[right]}, {middle, A[middle]} };
 
 	sort(arr, arr + 3, Comparator);
 
 	pivot = arr[1].index;
+
 	int position = partition(A, left, right, pivot);
 
 	if (position + 1 == k) {
 		return A[position];
 	}
 	else if (position + 1 < k) {
-		SelectionWithMedianOfThreePivot(A, position, right, k);
+		if (position + 1 <= right) {
+			return SelectionWithMedianOfThreePivot(A, position + 1, right, k);
+		}
+		return INT_MAX;
 	}
 	else {
-		SelectionWithMedianOfThreePivot(A, left, position, k);
+		if (position - 1 >= left) {
+			return SelectionWithMedianOfThreePivot(A, left, position - 1, k);
+		}
+		
+		return INT_MIN;
 	}
 
 }
 
 int main()
 {
+	srand(static_cast<long unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count()));
 	
-	vector<int> A, B;
-				
+	vector<int> A, B;			
 	int vector_size;
 	cout << "Enter required vector size: ";
 	cin >> vector_size;
